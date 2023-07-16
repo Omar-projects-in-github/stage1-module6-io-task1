f972744319ef5f4d3ae287ff5eb3a8e002a482d1
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-17 00:09:24 +0600

    FileReader class finally has been changed so that it works

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,54 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try {
+            String fileData = readFile(file);
+            Map<String, String> keyValuePairs = parseData(fileData);
+            return createProfile(keyValuePairs);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    private String readFile(File file) throws IOException {
+        StringBuilder fileContent = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                fileContent.append(line).append("\n");
+            }
+        }
+        return fileContent.toString();
+    }
+
+    private Map<String, String> parseData(String fileData) {
+        Map<String, String> keyValuePairs = new HashMap<>();
+        String[] lines = fileData.split("\n");
+        for (String line : lines) {
+            int colonIndex = line.indexOf(':');
+            if (colonIndex != -1) {
+                String key = line.substring(0, colonIndex);
+                String value = line.substring(colonIndex + 1).trim();
+                keyValuePairs.put(key, value);
+            }
+        }
+        return keyValuePairs;
+    }
+
+    private Profile createProfile(Map<String, String> keyValuePairs) {
+        Profile profile = new Profile();
+        profile.setName(keyValuePairs.get("Name"));
+        profile.setAge(Integer.valueOf(keyValuePairs.get("Age")));
+        profile.setEmail(keyValuePairs.get("Email"));
+        profile.setPhone(Long.valueOf(keyValuePairs.get("Phone")));
+        return profile;
     }
 }
